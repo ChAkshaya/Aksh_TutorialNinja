41b2388772a2a6e01a202e313f4d9730249318b1
Akshaya <akshayareddy987@gmail.com>
2024-08-23 11:49:52 -0500

    Added Shopping Cart Test cases

--- Aksh_Se_001/src/test/java/ShoppingCartPack/ShoppingCartFunctionality.java
+++ Aksh_Se_001/src/test/java/ShoppingCartPack/ShoppingCartFunctionality.java
@@ -1,5 +1,45 @@
 package ShoppingCartPack;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class ShoppingCartFunctionality {
+	
+	@Test
+	public void navigateToShoppingCartFromSuccessMessage() throws InterruptedException {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://tutorialsninja.com/demo/");
+		
+		WebElement searchBox = driver.findElement(By.name("search"));
+		searchBox.sendKeys("HP");
+		
+		WebElement searchIcon = driver.findElement(By.xpath("//span[@class = 'input-group-btn']/child::button"));
+		searchIcon.click();
+		
+		WebElement productDisplay = driver.findElement(By.linkText("HP LP3065"));
+		productDisplay.click();
+		
+		WebElement addToCartButton = driver.findElement(By.id("button-cart"));
+		addToCartButton.click();
+		
+		Thread.sleep(3000);
+		
+		WebElement shoppingCartLinkOnSuccessMSg = driver.findElement(By.xpath("//div[contains(@class, 'alert-success')]//a[contains(text(), 'shopping cart')]"));
+		shoppingCartLinkOnSuccessMSg.click();
+		
+		String actualShoppingCartLink = driver.getCurrentUrl();
+		System.out.println(actualShoppingCartLink);
+		String expectedShoppingCartLink = "https://tutorialsninja.com/demo/index.php?route=checkout/cart";
+		
+		Assert.assertEquals(actualShoppingCartLink, expectedShoppingCartLink);
+		
+		driver.quit();
+		
+	}
 
 }
